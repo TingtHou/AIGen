f44b7a2073bb47157538a7acd191bdca9161a89d
Hou Tingting <Hou591102@outlook.com>
2019-01-25 23:25:41 +0800

    Initial

--- MINQUE/cuMatrixSample.h
+++ MINQUE/cuMatrixSample.h
@@ -5,9 +5,9 @@
 #include <Eigen/Dense>
 #include "cuMatrix.h"
 using Eigen::MatrixXd;
-#define M 1
+#define M 1000
 #define N 5000
-#define S 10
+#define S 1000
 void cuMatrixInv_timetest();
 void cuMatrixINVTest();
 void cuMatrixMultTest();
