2f0dab5c680e8ec13005bfb8107de51c7dc16286
TingtHou <tingtinghou@ufl.edu>
2020-02-13 13:26:23 -0500

    Change name from rln_nmq

---
+++ KNN/include/MINQUE1.h
@@ -0,0 +1,25 @@
+#pragma once
+#define EIGEN_USE_MKL_ALL
+
+#include "MinqueBase.h"
+#include "CommonFunc.h"
+#include "ToolKit.h"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "easylogging++.h"
+#include <mkl.h>
+#include <thread>
+#include "mkl_types.h"
+#include "mkl_cblas.h"
+
+class minque1 :
+	public MinqueBase
+{
+public:
+	minque1(int DecompositionMode, int altDecompositionMode, bool allowPseudoInverse);
+	void estimateVCs();
+	~minque1();
+private:
+};
+
