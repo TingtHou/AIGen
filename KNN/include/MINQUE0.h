963dc129abee82193db63455e5af56f9fac182cc
TingtHou <tingtinghou@ufl.edu>
2020-02-13 13:25:57 -0500

    MIQNUE(0) function

---
+++ KNN/include/MINQUE0.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "MinqueBase.h"
+#include "CommonFunc.h"
+#include "ToolKit.h"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "easylogging++.h"
+#include <mkl.h>
+#include <thread>
+#include "mkl_types.h"
+#include "mkl_cblas.h"
+
+class MINQUE0 :
+	public MinqueBase
+{
+public:
+	MINQUE0(int DecompositionMode, int altDecompositionMode, bool allowPseudoInverse);
+	void estimateVCs();
+	~MINQUE0();
+private:
+};
+
