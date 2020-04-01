1ac9a8ac8a1a67d03e3908543a53b3515d6a0006
TingtHou <tingtinghou@ufl.edu>
2020-04-01 17:16:57 -0400

    same with cu

--- KNN/include/cuMatrix.cuh
+++ KNN/include/cuMatrix.cuh
@@ -1,22 +1,20 @@
+#pragma once
 #include <cuda.h>
-#include <cuda_runtime.h>
-#include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
-#include <device_functions.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <cmath>
-#include "helper_cuda.h"
-__global__ void cuElementInverse_kernel(double *A, double *A_inv, const int nrows);
+__global__ void cuElementInverse_kernel(float*A, float*A_inv, const int nrows);
 //__global__ double cuMatrixCompare_kernel(double *A, double *B, double *residual);
-void cuElementInverse(double *A, double *A_inv, const int nrows); //api
+void cuElementInverse(float*A, float*A_inv, const int nrows); //api
 //bool cuMatrixCompare(double *A, double *B, int nrow, int row);
-__global__ void cuGetupperTriangular_kernel(double *A, int nrows);
-void cuGetupperTriangular(double * A, double *A_Tri, int nrows); //api
+//__global__ void cuGetupperTriangular_kernel(double *A, int nrows);
+//void cuGetupperTriangular(double * A, double *A_Tri, int nrows); //api
 
 __global__ void cuVectorCwiseProduct_kernel(float* A, float* B, float *C, int size);
 //__global__ double cuMatrixCompare_kernel(double *A, double *B, double *residual);
 void cuVectorCwiseProduct(float* A, float* B, float* C, int size); //api
+
+__global__ void cuVectorCwiseMinus_kernel(float* A, float* B, float* C, int size);
+//__global__ double cuMatrixCompare_kernel(double *A, double *B, double *residual);
+void cuVectorCwiseMinus(float* A, float* B, float* C, int size); //api
 
 __global__ void cuTranspose_kernel(float* A, float* At, int width, int height);
 void cuTranspose(float* A, float* At, int width, int height);
