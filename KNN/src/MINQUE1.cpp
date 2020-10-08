4584ed1d83c21e69039fe02d340533bcb9d1836e
TingtHou <tingtinghou@ufl.edu>
2020-10-08 16:59:35 -0400

    add log output

--- KNN/src/MINQUE1.cpp
+++ KNN/src/MINQUE1.cpp
@@ -20,29 +20,33 @@ void minque1::estimateVCs()
 		W = Eigen::VectorXf(nVi);
 		W.setOnes();
 	}
+	std::vector<float*> pr_vi_list(nVi);
 	float* pr_VW = VW.data();
 	float* pr_Identity = Identity.data();// Vi.at(nVi - 1)->data();
+	LOG(INFO) << "Calcuate VW";
 	//std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < nVi; i++)
 	{
 		float* pr_Vi = (*Vi[i]).data();
+		pr_vi_list[i] = Vi[i]->data();
 		cblas_sgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, nind, nind, nind, W[i], pr_Vi, nind, pr_Identity, nind, 1, pr_VW, nind);
 	}
 	Identity.resize(0, 0);
+	LOG(INFO) << "Inverse VW";
 	int status=Inverse(VW, Decomposition, altDecomposition, allowPseudoInverse);
 	CheckInverseStatus("V matrix",status, allowPseudoInverse);
-	std::vector<Eigen::MatrixXf> RV(nVi);
+//	std::vector<Eigen::MatrixXf> RV(nVi);
 	Eigen::VectorXf Ry(nind);
-	std::vector<float*> pr_rv_list(nVi);
-	std::vector<float*> pr_vi_list(nVi);
-	#pragma omp parallel for
-	for (int i = 0; i < nVi; i++)
-	{
-		RV[i].resize(nind, nind);
-		RV[i].setZero();
-		pr_rv_list[i] = RV[i].data();
-		pr_vi_list[i] = Vi[i]->data();
-	}
+//	std::vector<float*> pr_rv_list(nVi);
+
+//	#pragma omp parallel for
+	//for (int i = 0; i < nVi; i++)
+//	{
+	//	RV[i].resize(nind, nind);
+	//	RV[i].setZero();
+	//	pr_rv_list[i] = RV[i].data();
+//		pr_vi_list[i] = Vi[i]->data();
+//	}
 	if (ncov == 0)
 	{
 		float* pr_Y = Y.data();
@@ -63,45 +67,59 @@ void minque1::estimateVCs()
 		float* pr_B = B.data();
 		float* pr_Xt_B = Xt_B.data();
 		float* pr_inv_XtB_Bt = inv_XtB_Bt.data();
+		LOG(INFO) << "calc B=inv(VW)*X";
 		//B=inv(VW)*X
 		cblas_sgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, nind, X.cols(), nind, 1, pr_VW, nind, pr_X, nind, 0, pr_B, nind);
 		//XtB=Xt*B
+		LOG(INFO) << "calc XtB=Xt*B";
 		cblas_sgemm(CblasColMajor, CblasTrans, CblasNoTrans, X.cols(), X.cols(), nind, 1, pr_X, nind, pr_B, nind, 0, pr_Xt_B, X.cols());
 		//inv(XtB)
+		LOG(INFO) << "inverse XtB";
 		int status = Inverse(Xt_B, Cholesky , SVD, false);
 		CheckInverseStatus("P matrix",status, false);
 		//inv_XtB_Bt=inv(XtB)*Bt
+		LOG(INFO) << "calc inv_XtB_Bt=inv(XtB)*Bt";
 		cblas_sgemm(CblasColMajor, CblasNoTrans, CblasTrans, X.cols(), nind, X.cols(), 1, pr_Xt_B, X.cols(), pr_B, nind, 0, pr_inv_XtB_Bt, X.cols());
 		//inv_VM=inv_VM-B*inv(XtB)*Bt
+		LOG(INFO) << "calc inv_VM=inv_VM-B*inv(XtB)*Bt";
 		cblas_sgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, nind, nind, X.cols(), -1, pr_B, nind, pr_inv_XtB_Bt, X.cols(), 1, pr_VW, nind);
 		//MKL_free(pr_VWp);
+		LOG(INFO) << "calc Ry = VW *Y; ";
 		cblas_sgemv(CblasColMajor, CblasNoTrans, nind, nind, 1, pr_VW, nind, pr_Y, 1, 0, pr_RY, 1);
 		//Ry = VW *Y; 
 	}
-	int nthread = omp_get_max_threads();
-	//	printf("Thread %d, Max threads for mkl %d\n", omp_get_max_threads(), mkl_get_max_threads());
-	int threadInNest = 1;
-	if (nthread != 1)
-	{
-		int tmp_thread = nthread > nVi ? nVi : nthread;
-		omp_set_num_threads(tmp_thread);
-		threadInNest = nthread/ nVi;
-	}
-	#pragma omp parallel for shared(pr_VW)
-	for (int i = 0; i < nVi; i++)
-	{
-		omp_set_num_threads(threadInNest);
-		cblas_ssymm(CblasColMajor, CblasLeft, CblasUpper, nind, nind, 1, pr_VW, nind, pr_vi_list[i], nind, 0, pr_rv_list[i], nind);
-	}
-	omp_set_num_threads(nthread);
+	LOG(INFO) << "calc u";
 	Eigen::VectorXf u(nVi);
 	u.setZero();
 	#pragma omp parallel for shared(Ry)
 	for (int i = 0; i < nVi; i++)
 	{
-		Eigen::VectorXd Ry_Vi_Ry = (Ry.cwiseProduct((*Vi[i]) *Ry)).cast<double>();
+		Eigen::VectorXd Ry_Vi_Ry = (Ry.cwiseProduct((*Vi[i]) * Ry)).cast<double>();
 		u[i] = (float)Ry_Vi_Ry.sum();
 	}
+//	int nthread = omp_get_max_threads();
+	//	printf("Thread %d, Max threads for mkl %d\n", omp_get_max_threads(), mkl_get_max_threads());
+//	int threadInNest = 1;
+//	if (nthread != 1)
+//	{
+//		int tmp_thread = nthread > nVi ? nVi : nthread;
+//		omp_set_num_threads(tmp_thread);
+//		threadInNest = nthread/ nVi;
+//	}
+//	#pragma omp parallel for shared(pr_VW)
+	LOG(INFO) << "calc RV";
+	Eigen::MatrixXf RV(nind, nind);
+	for (int i = 0; i < nVi; i++)
+	{
+	//	RV[i].resize(nind, nind);
+		RV.setZero();
+//		omp_set_num_threads(threadInNest);
+		cblas_ssymm(CblasColMajor, CblasLeft, CblasUpper, nind, nind, 1, pr_VW, nind, pr_vi_list[i], nind, 0, RV.data(), nind);
+		(*Vi[i]) = RV;
+	}
+	RV.resize(0, 0);
+//	omp_set_num_threads(nthread);
+	LOG(INFO) << "calc F";
 	Eigen::MatrixXf F(nVi, nVi);
 	//Eigen::MatrixXf F_(nVi, nVi);
 	#pragma omp parallel for shared(RV,F)
@@ -109,11 +127,12 @@ void minque1::estimateVCs()
 	{
 		int i = k / nVi, j = k % nVi;
 		if (j < i) i = nVi - i, j = nVi - j - 1;
-		Eigen::MatrixXf RVij = (RV[i].transpose().cwiseProduct(RV[j]));
+		Eigen::MatrixXf RVij = (*Vi[i]).transpose().cwiseProduct((*Vi[j]));
 		double sum = RVij.cast<double>().sum();
 		F(i, j) = (float)sum;
 		F(j, i) = F(i, j);
 	}
+	LOG(INFO) << "inverse F";
 	status = Inverse(F, Cholesky,  SVD, true);
 	CheckInverseStatus("S matrix",status,true);
 	vcs = F * u;
